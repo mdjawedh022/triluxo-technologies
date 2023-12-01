b18a02a3546fec7dd3ecb39b5f9e54b580b9a793
Md Jawed <mdjawedh22@gmail.com>
2023-12-01 18:43:20 +0530

    post method complete

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express =require("express");
 const app=express();
 const cors=require("cors");
-const {connection} =require("./config/db")
+const {connection} =require("./config/db");
+const { getRouter } = require("./routes/bookGet.routes");
+const postRouter = require("./routes/bookPost.routes");
 require("dotenv").config();
-
 
 app.get("/",(req,res)=>{
     res.send("welcome to book library!");
 })
+
+app.use(express.json());
+app.use(cors());
+app.use("/api",getRouter);
+app.use("/api", postRouter);
+
+
+
+
 
 app.listen(process.env.port,async()=>{
 try{
---
+++ routes/bookPost.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { BookModel } = require("../model/book.model");
+
+const postRouter = express.Router();
+
+postRouter.post("/add", async (req, res) => {
+  try {
+    const { image, title, category, price, description } = req.body;
+    const newBook = new BookModel({
+      image,
+      title,
+      category,
+      price,
+      description,
+    });
+    await newBook.save();
+    res.send({ msg: "Book created successfully" });
+  } catch (err) {
+    res.send({ msg: "Internal server error" });
+  }
+});
+
+module.exports=postRouter;
